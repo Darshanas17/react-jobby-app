c0946538eefa1420fd0c03abfc83cf104c3483bb
Darshan <darshanas1234@gmail.com>
2025-04-25 20:15:14 +0530

    updated to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
-import Login from './components/Login'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import ProtectedRoute from './components/ProtectedRoute'
-import NotFound from './components/NotFound'
-import JobItemDetails from './components/JobItemDetails'
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./components/NotFound";
+import JobItemDetails from "./components/JobItemDetails";
 
-import './App.css'
+import "./App.css";
 
 const App = () => (
   <Switch>
@@ -17,6 +17,6 @@ const App = () => (
     <Route exact path="/not-found" component={NotFound} />
     <Redirect to="/not-found" />
   </Switch>
-)
+);
 
-export default App
+export default App;
